d9101d8c427ae8a91a8162b87e249660fee4f452
Thành Trung <npttrung2711@gmail.com>
2024-12-23 16:06:36 +0700

    + Init project

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Thành Trung on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
